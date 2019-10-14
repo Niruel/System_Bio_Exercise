e94c4a105f046cee8d73e71e09138470dc9918f1
Nicholas Ruppel <npruppel@gmail.com>
2019-10-14 19:55:00 +0900

    Comments in code

--- 26001904476_Q2_Week3.py
+++ 26001904476_Q2_Week3.py
@@ -1,13 +1,18 @@
+"""
+Created By: Nicholas Ruppel
+System Biology Exercise Practice
+Week 3 Exercise
+10/14/19
+"""
 import  string
 import  random
 
 
 def randomwordOdd(length):
-    String = set(string.ascii_lowercase)
-    StrigH = string.ascii_uppercase
-    Oddchar = set(StrigH[0:25:2])
-    String.update(Oddchar)
-    print(String)
+    String = set(string.ascii_lowercase) # is a set of lower case ascii characters
+    StrigH = string.ascii_uppercase #make an list of ascii uppercase characters
+    Oddchar = set(StrigH[0:25:2]) #make the upper case ascii into a set and set it to a varible
+    String.update(Oddchar) #add the odd characters to the exsisting set
     mystring = ""
     i = 0
     while i < length:  # while the length has not run out do something
@@ -16,8 +21,8 @@ def randomwordOdd(length):
         for str in String:
             count += 1
             if count == r:
-                if str.isupper():
-                    str = str.lower()
+                if str.isupper(): #if the value chosen is an upper case odd
+                    str = str.lower() #make it lowercase
                 mystring += str  # let the varibile increment
         i += 1
     return mystring  # return final string
--- 26001904476_week3.py
+++ 26001904476_week3.py
@@ -1,7 +1,8 @@
 """
+Created By: Nicholas Ruppel
 System Biology Exercise Practice
 Week 3 Exercise
-10/12/19
+10/14/19
 """
 import string
 import random
@@ -9,9 +10,9 @@ import random
 
 # First function To deal with random number generation
 def randomword3(length):
-    String = set(string.ascii_lowercase)  # array of ascii characters
+    String = set(string.ascii_lowercase)  # Set of ascii characters
 
-    mystring = " "
+    mystring = " " # my string is the created varible to hold info
     i = 0
     while i < length:  # while the length has not run out do something
         r = random.randint(1, len(String))  # set r to a random int in the string array
@@ -22,30 +23,6 @@ def randomword3(length):
                 mystring += str  # let the varibile increment
         i += 1
     return mystring  # return final string
-
-
-def randomwordOdd(length):
-    String = set(string.ascii_lowercase)
-    StrigH = string.ascii_uppercase
-    Oddchar = set(StrigH[0:25:2])
-    String.update(Oddchar)
-    print(String)
-    mystring = ""
-    i = 0
-    while i < length:  # while the length has not run out do something
-        r = random.randint(1, len(String))  # set r to a random int in the string array
-        count = 0
-        for str in String:
-            count += 1
-            if count == r:
-                if str.isupper():
-                    str = str.lower()
-                mystring += str  # let the varibile increment
-        i += 1
-    return mystring  # return final string
-
 
 finalString1 = randomword3(10)
-finalString2 = randomwordOdd(10)
 print(finalString1)
-print(finalString2)
