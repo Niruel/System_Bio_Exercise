9f71252c9ff2b8be7d05c860ab63c590513e3d1b
Niruel <npruppel@gmail.com>
2019-10-19 21:57:59 +0900

    Finish home work(uncommented)

--- 26001904476_Week04_1.py
+++ 26001904476_Week04_1.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 startTime = timeit.default_timer()
-def Mandelbrot(n,itteration_num,m_origin=" "):
+def Mandelbrot(n, itteration_num, m_origin=" "):
     
    
     M = np.zeros([n,n],int)
@@ -21,7 +21,7 @@ def Mandelbrot(n,itteration_num,m_origin=" "):
     plt.imshow(M,origin=m_origin,extent=(-2,0.5,-1,1))
     plt.gray()
     plt.show()
-Mandelbrot(32,40,"upper")
+Mandelbrot(200,100,"upper")
 stopTimer=timeit.default_timer()
 
 print("This took " + str(stopTimer-startTime)[0:5] + " Seconds to run")
@@ -29,23 +29,4 @@ print("This took " + str(stopTimer-startTime)[0:5] + " Seconds to run")
 
 
 
-#dType.GetHOMEParams(api)
-#dType.SetHOMECmd(api, temp, isQueued=0)
-dType.SetPTPJointParams(api, 200, 300, 500, 100, 400, 200, 600, 400, isQueued=0)
-dType.GetPTPJointParams(api)
 
-dType.SetPTPCoordinateParams(api, 100, 100, 100,  100,  isQueued=0)
-pos = dType.GetPose(api)
-moveX=0;moveY=0;moveZ=10;moveFlag=-1
-x = pos[0]
-y = pos[1]
-z = pos[2]
-rHead = pos[3]
-
-while True:
-moveFlag *= -1
-   	for i in range(5):
-        dType.SetPTPCmd(api, dType.PTPMode.PTPMOVLXYZMode, x+moveX, y+moveY, z+moveZ, rHead, 1)
-        moveX += 10 * moveFlag
-        dType.SetPTPCmd(api, dType.PTPMode.PTPMOVLXYZMode, x+moveX, y+moveY, z+moveZ, rHead, 1)
-        dType.SetPTPCmd(api, dType.PTPMode.PTPMOVLXYZMode, x+moveX, y+moveY, z, rHead, 1)
---
+++ 26001904476_Week04_2.py
@@ -0,0 +1,45 @@
+import timeit
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def ElephantValley():
+    Equation(1000,100,0.24,0.4,-0.1,0.1)
+
+def SeahorseValley():
+    Equation(1000,100,-0.85,-0.65,0.0,0.2)
+
+def Mandelbrot():   
+    Equation(1000,100,-2,0.5,-1,1)
+
+def Equation(n,itteration_num,xMin,xMax,yMin,yMax):
+    M = np.zeros([n,n],int)
+    xval = np.linspace(xMin,xMax,n)
+    yval = np.linspace(yMin,yMax,n)
+    for u,x in enumerate(xval):
+        for v,y in enumerate(yval):
+            z= 0+0j
+            C =complex(x,y)
+            for n in range(itteration_num):
+                z=z*z + C
+                if abs(z)> 2.0:
+                    M[v,u]=1
+                    break
+    plt.imshow(M,origin="lower",extent=(xMin,xMax,yMin,yMax))
+   
+if __name__ == "__main__":
+
+    u_input_Text = input("Choose which plot you would like to see [1-3]:")
+    u_input_num=int(u_input_Text)
+    if(u_input_num==1):
+        Mandelbrot()
+        print("ne1")
+    elif(u_input_num==2):
+        ElephantValley()
+    elif(u_input_num==3):
+        SeahorseValley()
+
+    plt.gray()
+    plt.show()
+
+
