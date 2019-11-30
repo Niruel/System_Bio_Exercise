662b9fd7d92dfb747888ea5d0f95f6b1cea78fd7
Nicholas Ruppel <npruppel@gmail.com>
2019-11-30 15:13:45 +0900

    Added fitness function and fixed week 9

--- 26001904476_Week10_1.py
+++ 26001904476_Week10_1.py
@@ -1,9 +1,9 @@
 """
 Created By: Nicholas Ruppel
 System Biology Exercise Practice
-Week 9 Exercise
-Genetic Algorithm
-2019/11/27
+Week 10 Exercise
+Genetic Algorithm Fitness Function Creation
+2019/11/30
 """
 import random
 import string
@@ -12,22 +12,63 @@ import numpy as np
 target = "Nicholas"
 dnaLen= len(target)
 populationSize = 20
-generations =5000
+generations =50000
 mutationChance = 100
 
+"""
+
+"""
 def randomGene():
     return random.choice(string.printable)
+"""
 
+"""
 def initPopulation():
     initPop = [] #create the list to be returned
     for i in range(populationSize): #Check population size
         initPop.append(''.join(random.choice(string.printable)for i in range(dnaLen)))
     return initPop
-#This function is not used at this moment
-def fitnessFunc(competingDNA):
+"""
+
+"""
+def fitnessFunc1(competingDNA):
     fitness = 0
+    
+    for i in range(dnaLen):
+        temp = abs(ord(competingDNA[i])-ord(target[i]))
+        fitness +=temp
     return fitness
-#Mutate the target and competing DNA
+"""
+
+"""
+def fitnessFunc2(competingDNA):
+    fitness = 0
+    for i in range(dnaLen):
+        temp = (ord(competingDNA[i]) - ord(target[i]))**2
+        fitness +=temp
+
+    return fitness
+"""
+
+"""
+def fitnessFunc3(competingDNA):
+    fitness = 0
+    competingDNA_List = [ord(c) for c in competingDNA]
+    DNATarget_List = [ord(t) for t in target]
+    for i in range(dnaLen):
+           if competingDNA_List[i] != DNATarget_List[i]:
+               fitness+=1
+
+    return fitness
+"""
+
+"""
+def fitnessFunc(competingDNA):
+    return fitnessFunc3(competingDNA)
+
+"""
+
+"""
 def mutation(competingDNA, mutationChance):
     mutatedDNA = ""
     rand = random.randint(1, mutationChance)
@@ -41,23 +82,32 @@ def mutation(competingDNA, mutationChance):
     else:
         mutatedDNA = competingDNA
     return mutatedDNA
-#Recombine target and competing DNA
+
+"""
+
+"""
 def recombination(competingDNA_1,competingDNA_2):
     crossOverPoint = random.randint(0, dnaLen - 1)
     DNAout_1 = competingDNA_1[:crossOverPoint] + competingDNA_2[crossOverPoint:]
     DNAout_2 = competingDNA_2[:crossOverPoint] + competingDNA_1[crossOverPoint:]
     return (DNAout_1, DNAout_2)
-#Weighted choice is to check probabilty
+"""
+
+"""
 def weightedDNAchoice(competingDNAfitnessPairs):
     probs = [competingDNAfitnessPairs[i][1] for i in range(len(competingDNAfitnessPairs))]
     probs = np.array(probs)
     probs /= probs.sum()
     return competingDNAfitnessPairs[np.random.choice(len(competingDNAfitnessPairs),1,p = probs)[0]][0]
 
+"""
+
+"""
 currentpop = initPopulation()
 for i in range(generations):
     lastfitnessarray = []
     for k in currentpop:
+        #Change fitnessfunc to which function you want 1, 2, 3
         lastfitnessarray.append(fitnessFunc(k))
 
     print("The last fittest DNA", i , "is---", currentpop[lastfitnessarray.index(min(lastfitnessarray))],
@@ -65,6 +115,7 @@ for i in range(generations):
 
     populationWeighted = []
     for individuals in currentpop:
+        
         individualsPenalty = fitnessFunc(individuals)
         if individualsPenalty == 0:
             DNAFitnessPair = (individuals, 1.0)
@@ -79,14 +130,16 @@ for i in range(generations):
 
         fitnessDNA_1,fitnessDNA_2 =recombination(fitnessDNA_1,fitnessDNA_2)
 
+
         fitnessDNA_1=mutation(fitnessDNA_1,mutationChance)
         fitnessDNA_2=mutation(fitnessDNA_2,mutationChance)
 
         currentpop.append(fitnessDNA_1)
         currentpop.append(fitnessDNA_2)
 
-    lastfitnessarray = []
+lastfitnessarray = []
 for g in currentpop:
+    #Change fitnessfunc to which function you want 1, 2, 3
     lastfitnessarray.append(fitnessFunc(g))
 
 print("fitesst string at: ", generations, " is", currentpop[lastfitnessarray.index(min(lastfitnessarray))])
